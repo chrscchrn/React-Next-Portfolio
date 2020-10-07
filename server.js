a285d4cd174cdbde7931511d5f0cdc74f8ec14ce
chrscchrn <cochranschris@gmail.com>
2020-10-07 17:08:40 -0500

    express hosting Next server, need to make email api route

---
+++ server.js
@@ -0,0 +1,28 @@
+const next = require('next');
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const server = express();
+const viewRoutes = require('./viewRoutes');
+server.use(express.static('public'));
+
+app.prepare().then(() => {
+  viewRoutes(server, app);
+
+  server.get('*', (req, res) => {
+    app.render(req, res, '/');
+  });
+
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(' Next.js "server" ready on port', PORT);
+  });
+}).catch((ex) => {
+  console.error(ex.stack); // eslint-disable-line no-console
+  process.exit(1);
+});
+
+
+module.exports = app;
---
+++ viewRoutes.js
@@ -0,0 +1,25 @@
+module.exports = (server, app) => {
+  server.get('/about/', (req, res) => {
+    app.render(req, res, '/about');
+  });
+
+  server.get('/about/:page', (req, res) => {
+    app.render(req, res, '/about');
+  });
+
+  server.get('/about', (req, res) => {
+    app.render(req, res, '/about');
+  });
+
+  server.get('/contact/', (req, res) => {
+    app.render(req, res, '/contact');
+  });
+
+  server.get('/contact/:page', (req, res) => {
+    app.render(req, res, '/contact');
+  });
+
+  server.get('/contact', (req, res) => {
+    app.render(req, res, '/contact');
+  });
+};
