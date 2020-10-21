56af4ee5713d731487b0c088af34a116996250c3
chrscchrn <cochranschris@gmail.com>
2020-10-20 21:28:54 -0500

    adding resume

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import { CssBaseline } from '@material-ui/core';
+import { Button, CssBaseline } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Navbar from '../components/navbar';
